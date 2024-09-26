ad3640f24b4d257b793450ade9044255c223a6d5
pensivetraveler <pensivetraveler@gmail.com>
2024-09-26 10:56:46 +0900

    feat: base updated

--- application/controllers/admin/Common.php
+++ application/controllers/admin/Common.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Common extends MY_Controller_WEB
+class Common extends MY_Controller_ADM
 {
 	function __construct()
 	{
