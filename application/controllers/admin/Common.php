938a1417cb485e0021d2068dedc1027a6a44ab69
pensivetraveler <pensivetraveler@gmail.com>
2025-02-03 15:55:14 +0900

    feat: extends to MY_Builder_WEB

--- application/controllers/admin/Common.php
+++ application/controllers/admin/Common.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Common extends MY_Controller_Builder
+class Common extends MY_Builder_WEB
 {
 	public string $token;
 	public object $userData;
--- application/controllers/web/Common.php
+++ application/controllers/web/Common.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Common extends MY_Controller_Builder
+class Common extends MY_Builder_WEB
 {
 	public string $token;
 	public object $userData;
