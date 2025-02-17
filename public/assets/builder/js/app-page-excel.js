d075b215dbd4f243c52f6321d25dbcd52027fe56
pensivetraveler <pensivetraveler@gmail.com>
2025-02-17 12:17:25 +0900

    feat: changed

--- public/assets/builder/js/app-page-excel.js
+++ public/assets/builder/js/app-page-excel.js
@@ -1,18 +1,140 @@
+function emitErrorMessage(response) {
+	let key = Object.keys(response.data)[0];
+	let targetData = response.data[key];
+
+	let errorCount = 0;
+	const index = getNodeIndexInParent(document.querySelector(`th[data-field="${key}"]`));
+	if(index !== -1) {
+		for(const td of document.querySelectorAll(`#inline-editable tbody tr:not(.submit-completed) td:nth-child(${index+1})`)) {
+			if(td.querySelector('span').innerHTML === targetData) {
+				td.classList.add('edit-required');
+				errorCount++;
+			}
+		}
+
+		document.getElementById('totalErrorCount').innerText = errorCount.toString();
+		document.getElementById('btnErrorFind').removeAttribute('disabled');
+		document.getElementById('excelFormSubmit').setAttribute('disabled', 'disabled');
+	}
+
+	let filtered = excelHeaders.filter(item => item.field === key);
+	if(filtered.length) key = filtered[0].label;
+
+	showAlert({
+		type : 'error',
+		html: `<p>${response.msg}</p><p class="text-danger mt-2 mb-0">(${key} : ${targetData})</p>`,
+	});
+}
+
+function getEditableColumns(tableSelector, excludeIndices = [0]) {
+	let editableColumns = [];
+	let columnCount = $(tableSelector + " thead th").length; // 전체 컬럼 개수
+	excludeIndices.push(columnCount - 1);
+	$(tableSelector + " thead th").each(function (index) {
+		if (!excludeIndices.includes(index)) { // 제외할 컬럼(기본 ID 컬럼) 제외
+			editableColumns.push([index, "col" + index]);
+		}
+	});
+	return editableColumns;
+}
+
+async function validateData(dataArray, chunkSize = 50) {
+	return new Promise((resolve) => {
+		let index = 0;
+		let valid = true;
+
+		function sendChunk() {
+			if (!valid || index >= dataArray.length) {
+				resolve(valid); // 모든 요청이 끝나면 resolve
+				return;
+			}
+
+			let chunk = dataArray.slice(index, index + chunkSize);
+			index += chunkSize;
+
+			$.ajax({
+				url: common.API_URI + '/excelValidate',
+				type: "POST",
+				data: JSON.stringify(chunk),
+				contentType: "application/json",
+				success: function(response) {
+					console.log("Chunk sent successfully");
+					sendChunk(); // 다음 청크 보내기
+				},
+				error: function(xhr) {
+					console.log(xhr.responseJSON.data)
+					if([404,409].includes(Math.floor(xhr.responseJSON.code/10))) {
+						emitErrorMessage(xhr.responseJSON);
+					}else{
+						showAlert({
+							type : 'error',
+							text: xhr.responseJSON.msg,
+						});
+					}
+					valid = false;
+					resolve(valid); // 에러 발생 시 즉시 반환
+				}
+			});
+		}
+
+		sendChunk();
+	});
+}
+
+async function submitData(dataArray, chunkSize = 50) {
+	return new Promise((resolve) => {
+		let index = 0;
+		let valid = true;
+
+		function sendChunk() {
+			if (!valid || index >= dataArray.length) {
+				resolve(valid); // 모든 요청이 끝나면 resolve
+				showAlert({
+					type : 'success',
+					text: 'Registered Successfully',
+					callback: 'reload',
+				});
+				return;
+			}
+
+			let chunk = dataArray.slice(index, index + chunkSize);
+			index += chunkSize;
+
+			$.ajax({
+				url: common.API_URI+'/excelUpload',  // 서버의 저장 API URL
+				type: 'POST',
+				dataType: 'json',
+				data: JSON.stringify(editedData),
+				success: function(response) {
+					console.log(response)
+
+					for(const tr of document.querySelectorAll('#inline-editable tbody tr')){
+						if(parseInt(tr.id)>index && parseInt(tr.id)<=index+chunkSize){
+							tr.classList.add('submit-completed');
+						}
+					}
+				},
+				error: function(xhr) {
+					console.log(xhr)
+					if([404,409].includes(Math.floor(xhr.responseJSON.code/10))) {
+						emitErrorMessage(xhr.responseJSON);
+					}else{
+						showAlert({
+							type : 'error',
+							text: xhr.responseJSON.msg,
+						});
+					}
+				}
+			});
+		}
+
+		sendChunk();
+	});
+}
+
 $(function() {
 	const fieldList = excelHeaders.map(item => item.label);
 	const requiredList = excelHeaders.filter(item => item.required).map(item => item.label);
-
-	function getEditableColumns(tableSelector, excludeIndices = [0]) {
-		let editableColumns = [];
-		let columnCount = $(tableSelector + " thead th").length; // 전체 컬럼 개수
-		excludeIndices.push(columnCount - 1);
-		$(tableSelector + " thead th").each(function (index) {
-			if (!excludeIndices.includes(index)) { // 제외할 컬럼(기본 ID 컬럼) 제외
-				editableColumns.push([index, "col" + index]);
-			}
-		});
-		return editableColumns;
-	}
 
 	$("#inline-editable").on('click', 'button[value="remove"]', function () {
 		this.closest('tr').remove();
@@ -67,26 +189,31 @@ $(function() {
 			const table = document.getElementById('inline-editable');
 			const colCount = json[0].length;
 
+			const itemList = [];
+			for(let i = 1; i < json.length; i++) {
+				if(json[i].length === 0) continue;
+				itemList.push(json[i]);
+			}
+
 			// tbody
 			$(table).find('tbody').empty();
 			let totalRowCount = 0;
 			let totalErrorCount = 0;
 			const tbody = table.querySelector('tbody');
-			for(let i = 1; i < json.length; i++) {
-				if(json[i].length === 0) continue;
+			for(let i = 0; i < itemList.length; i++) {
 				const row = document.createElement('tr');
 				const init = document.createElement('td');
-				init.innerText = i;
+				init.innerText = i+1;
 				row.appendChild(init);
 				for(let j = 0; j < colCount; j++) {
-					const obj = json[i];
+					const obj = itemList[i];
 					const td = document.createElement('td');
 					td.innerText = obj[j]??'';
 
 					// required 확인
 					if(!td.innerText.length) {
 						if(requiredList.includes(fieldList[j])) {
-							td.classList.add('input-required');
+							td.classList.add('edit-required');
 							totalErrorCount++;
 						}
 					}
@@ -123,14 +250,14 @@ $(function() {
 					identifier: [0, "id"],
 					editable: getEditableColumns('#inline-editable')
 				},
-				onSuccess: function (data, textStatus, jqXHR) {
+				onSuccess: function (data, textStatus, xhr) {
 					console.log(data)
 					console.log(textStatus)
-					console.log(jqXHR)
+					console.log(xhr)
 				},
-				onSubmit: function(action, serializedData, jqXHR) {
+				onSubmit: function(action, serializedData, xhr) {
 					console.log("AJAX 요청 차단됨:", action, serializedData);
-					if (jqXHR) jqXHR.abort(); // 안전하게 AJAX 요청 중단
+					if (xhr) xhr.abort(); // 안전하게 AJAX 요청 중단
 					return false;
 				},
 			});
@@ -142,13 +269,13 @@ $(function() {
 				this.value = this.value.trim();
 				if(required) {
 					if(!this.value.length) {
-						if(!wrap.classList.contains('input-required')) wrap.classList.add('input-required');
+						if(!wrap.classList.contains('edit-required')) wrap.classList.add('edit-required');
 					}else{
-						if(wrap.classList.contains('input-required')) wrap.classList.remove('input-required');
+						if(wrap.classList.contains('edit-required')) wrap.classList.remove('edit-required');
 					}
 				}
 
-				if(!document.querySelectorAll('td.input-required').length) {
+				if(!document.querySelectorAll('td.edit-required').length) {
 					document.getElementById('excelFormSubmit').removeAttribute('disabled');
 					document.getElementById('btnErrorFind').setAttribute('disabled', 'disabled');
 				}
@@ -161,14 +288,13 @@ $(function() {
 	});
 
 	document.getElementById('btnErrorFind').addEventListener('click', function() {
-		const errors = document.querySelectorAll('td.input-required');
+		const errors = document.querySelectorAll('td.edit-required');
 		if(!errors.length) return;
 
 		const container = $('#inline-editable').parents('.table-responsive')[0];
 		const containerPos = container.getBoundingClientRect();
 		const columnPos = errors[0].getBoundingClientRect();
 		window.scrollTo(0, columnPos.y - 100);
-		console.log(columnPos)
 
 		setTimeout(function() {
 			const scrollX = Math.max(columnPos.x-containerPos.width, 0);
@@ -176,54 +302,22 @@ $(function() {
 		}, 500);
 	});
 
-	$('#excelFormSubmit').on('click', function() {
+	$('#excelFormSubmit').on('click', async function () {
 		let editedData = [];
 
-		$('#inline-editable tbody tr').each(function() {
+		$('#inline-editable tbody tr:not(.submit-completed)').each(function () {
 			let row = $(this);
 			let rowData = {};
 			$(row).find('td').each(function (k, v) {
-				if(k === 0 || k === $(row).find('td').length-1) return;
-				const columnName = excelHeaders[k-1].field;
+				if (k === 0 || k === $(row).find('td').length - 1) return;
+				const columnName = excelHeaders[k - 1].field;
 				rowData[columnName] = $(v).text();
 			});
 			editedData.push(rowData);
 		});
 
-		// AJAX를 통해 서버로 데이터 전송
-		$.ajax({
-			url: common.API_URI+'/excelUpload',  // 서버의 저장 API URL
-			type: 'POST',
-			dataType: 'json',
-			data: { data: editedData },
-			success: function(response) {
-				console.log(response)
-				showAlert({
-					type : 'success',
-					text: 'Registered Successfully',
-					callback: 'reload',
-				});
-			},
-			error: function(jqXHR) {
-				console.log(jqXHR)
-				if([404,409].includes(Math.floor(jqXHR.responseJSON.code/10))) {
-					let key = Object.keys(jqXHR.responseJSON.data)[0];
-					let targetData = jqXHR.responseJSON.data[key];
-					let filtered = excelHeaders.filter(item => item.field === key);
-					if(filtered.length) key = filtered[0].label;
-
-					showAlert({
-						type : 'error',
-						html: `<p>${jqXHR.responseJSON.msg}</p><p class="text-danger mt-2 mb-0">(${key} : ${targetData})</p>`,
-					});
-				}else{
-					showAlert({
-						type : 'error',
-						text: jqXHR.responseJSON.msg,
-					});
-				}
-			}
-		});
+		const valid = await validateData(editedData, 50);
+		if(valid) await submitData(editedData, 50);
 	});
 
 	$('#resetExcelForm').on('click', function() {
