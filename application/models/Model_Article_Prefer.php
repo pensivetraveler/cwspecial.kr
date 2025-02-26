8b88f681edaf44943e6516de8cd9e267a902a9d3
pensivetraveler <pensivetraveler@gmail.com>
2025-02-26 22:49:11 +0900

    feat: comment, article_prefer 관련 model 추가

--- application/models/Model_Article_Prefer.php
+++ application/models/Model_Article_Prefer.php
@@ -1,6 +1,24 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Model_Article_Prefer
+require_once __DIR__.'/Model_Common.php';
+
+class Model_Article_Prefer extends Model_Common
 {
+	public string  $table = 'article_prefer';
+	public string  $identifier = '';
+	public array   $primaryKeyList = ['article_id','user_id'];
+	public array   $uniqueKeyList = [];
+	public array   $notNullList = ['article_id','user_id','prefer_cd'];
+	public array   $nullList = [];
+	public array   $strList = ['prefer_cd'];
+	public array   $intList = ['article_id','user_id'];
+	public array   $fileList = [];
 
+	public bool    $isCreatedDt = true;
+
+	function __construct()
+	{
+		parent::__construct();
+	}
 }
