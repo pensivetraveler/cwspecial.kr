65092d64bd1ecd119923676ea2b87b4c7c00cdbe
pensivetraveler <pensivetraveler@gmail.com>
2025-02-28 06:53:41 +0900

    feat: setCookie, exp default value set

--- public/assets/base/js/utils.js
+++ public/assets/base/js/utils.js
@@ -134,7 +134,7 @@ function resizeScale() {
 
 /****************************************************/
 
-var setCookie = function(name, value, exp) {
+var setCookie = function(name, value, exp = 1) {
 	var date = new Date();
 	date.setTime(date.getTime() + exp*24*60*60*1000);
 	document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
