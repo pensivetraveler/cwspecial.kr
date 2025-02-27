baf7c734a054624c3fdf41efae8a5cb2d7b0e7ec
pensivetraveler <pensivetraveler@gmail.com>
2025-02-27 17:03:08 +0900

    feat: add withdraw btn

--- application/views/web/myInfo/edit.php
+++ application/views/web/myInfo/edit.php
@@ -1,1 +1,134 @@
-<?php
+<!-- Basic Layout -->
+<div class="row g-6 mb-6">
+	<nav aria-label="breadcrumb">
+		<ol class="breadcrumb"><?=get_admin_breadcrumbs($titleList)?></ol>
+	</nav>
+</div>
+<div class="row g-6 mb-6">
+	<div class="card mb-6">
+		<div class="card-header d-flex justify-content-between align-items-center">
+			<h5 class="mb-0"><?=lang('nav.'.$titleList[1])?> 등록</h5>
+		</div>
+		<div class="card-body">
+			<?php
+			echo form_open_multipart('', [
+				'id' => 'formRecord',
+				'class' => "add-new-record needs-validation form-type-{$formType}",
+				'onsubmit' => 'return false',
+			], [
+				'_mode' => $this->router->method,
+				'_event' => '',
+			]);
+			foreach ($formData as $item):
+				if($item['category'] === 'group'):
+					_view("admin/layout/form_{$formType}_group_".$item['view'], ['item' => $item]);
+				elseif($item['category'] === 'custom'):
+					_view("admin/layout/form_{$formType}_custom_".$item['view'], ['item' => $item]);
+				elseif($item['type'] === 'hidden'):
+					echo form_input(
+						[
+							'type' => $item['type'],
+							'name' => $item['field'],
+							'id' => $item['id'],
+						],
+						set_admin_form_value($item['field'], $item['default'], null),
+						$item['attributes'],
+					);
+				else:
+					?>
+					<div class="row mb-4 form-validation-unit">
+						<?=form_label(lang($item['label']), $item['id'], ['class' => 'col-sm-2 col-form-label'])?>
+						<div class="col-sm-10">
+							<div class="input-group input-group-merge">
+								<?php
+								echo get_admin_form_ico($item);
+								switch ($item['type']) {
+									case 'password' :
+										echo form_password(
+											[
+												'name' => $item['field'],
+												'id' => $item['id'],
+											],
+											set_admin_form_value($item['field'], $item['default'], null),
+											$item['attributes']
+										);
+										break;
+									case 'checkbox' :
+									case 'radio' :
+										echo get_admin_form_choice($item, $formType);
+										break;
+									case 'select' :
+										echo form_dropdown(
+											$item['field'],
+											$item['options'] ?? [],
+											set_admin_form_value($item['field'], $item['default'], null),
+											array_merge([
+												'id' => $item['id'],
+												'data-style' => 'btn-default'
+											], $item['attributes'])
+										);
+										break;
+									case 'textarea' :
+										echo form_textarea(
+											[
+												'name' => $item['field'],
+												'id' => $item['id'],
+												'rows' => $item['attributes']['rows']
+											],
+											set_admin_form_value($item['field'], $item['default'], null),
+											$item['attributes']
+										);
+										break;
+									case 'file' :
+										echo form_upload([
+											'name' => $item['field'],
+											'id' => $item['id'],
+										], $item['attributes']);
+										break;
+									default :
+										echo form_input(
+											[
+												'type' => $item['type'],
+												'name' => $item['field'],
+												'id' => $item['id'],
+											],
+											set_admin_form_value($item['field'], $item['default'], null),
+											$item['attributes']
+										);
+								}
+								?>
+								<?php
+								if($item['form_attributes']['with_btn']) {
+									switch ($item['form_attributes']['btn_type']) {
+										case 'dup_check' :
+											echo form_button([
+												'data-rel-field' => $item['field'],
+												'type' => 'button',
+												'class' => 'btn btn-outline-primary waves-effect btn-dup-check',
+											], lang('Check'), [
+												'onclick' => "checkDuplicate(this)",
+												'disabled' => 'disabled',
+											]);
+											break;
+									}
+								}
+								?>
+							</div>
+							<?=get_admin_form_text($item)?>
+							<?=get_admin_form_list_item($item, $formType)?>
+						</div>
+					</div>
+				<?php
+				endif;
+			endforeach;
+			?>
+			<div class="row">
+				<div class="col-sm-12 text-end">
+					<button type="button" class="btn btn-outline-dark waves-effect btn-withdraw"><?=lang('Withdraw')?></button>
+					<button type="submit" class="btn btn-primary waves-effect waves-light"><?=lang('Submit')?></button>
+				</div>
+			</div>
+			<?=form_close();?>
+		</div>
+	</div>
+</div>
