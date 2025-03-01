3c18784a6c188f72f98f109705c271d273e59ccf
pensivetraveler <pensivetraveler@gmail.com>
2025-03-02 00:20:52 +0900

    feat: arragne includes files

--- application/views/admin/includes/footer.php
+++ application/views/admin/includes/footer.php
@@ -7,20 +7,7 @@
 								<div
 									class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
 									<div class="text-body mb-2 mb-md-0">
-										© <?=date('Y')?> EDUPRIME
-										<!--
-										© <?=date('Y')?>, made with <span class="text-danger"><i class="tf-icons ri-heart-fill"></i></span> by
-										<a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
-										-->
-									</div>
-									<div class="d-none d-lg-inline-block">
-										<!--
-										<a
-											href="https://demos.pixinvent.com/materialize-html-admin-template/documentation/"
-											target="_blank"
-											class="footer-link me-4"
-										>Documentation</a>
-										-->
+										© <?=date('Y')?> <?=APP_NAME_EN?>
 									</div>
 								</div>
 							</div>
