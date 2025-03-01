809220ec86ba9f1f09c781e4220e078fe3c1a1ad
pensivetraveler <pensivetraveler@gmail.com>
2025-03-02 00:07:34 +0900

    feat: 내 작업물 페이지용 script 추가

--- application/views/web/includes/modal_append.php
+++
@@ -1,9 +0,0 @@
-                            <!-- Youtube Modal -->
-                            <div class="modal fade" id="youTubeModal" tabindex="-1" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered" role="document">
-                                    <div class="modal-content">
-                                        <iframe height="350" src="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- / Youtube Modal -->
