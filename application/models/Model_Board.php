586c3cf49667d0617b17896fc1926cd3548fac0e
pensivetraveler <pensivetraveler@gmail.com>
2025-03-03 00:12:46 +0900

    feat: add some columns

--- application/models/Model_Board.php
+++ application/models/Model_Board.php
@@ -9,13 +9,16 @@ class Model_Board extends Model_Common
 	public string  $identifier = 'board_id';
 	public array   $primaryKeyList = ['board_id'];
 	public array   $uniqueKeyList = [];
-	public array   $notNullList = ['board_name'];
-	public array   $nullList = [];
-	public array   $strList = ['board_name'];
-	public array   $intList = ['board_id'];
+	public array   $notNullList = ['board_id','board_name','board_cd'];
+	public array   $nullList = ['attach_max'];
+	public array   $strList = ['board_name','board_cd',];
+	public array   $intList = ['board_id','attach_max'];
 	public array   $fileList = [];
 
 	public bool    $isAutoincrement = true;
+	public bool    $isCreatedId = true;
+	public bool    $isCreatedDt = true;
+	public bool    $isUpdatedDt = true;
 
 	function __construct()
 	{
