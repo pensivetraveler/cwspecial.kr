542313e9fe4a59d454cbb07e333826ba4b0e1df8
pensivetraveler <pensivetraveler@gmail.com>
2025-03-04 02:59:59 +0900

    feat: 빌더 단일화로 안쓰는 view 제거

--- application/views/web/layout/index.php
+++
@@ -1,9 +0,0 @@
-<?php if(!isset($includes)) $includes = $this->config->config['web_base_includes_config']; ?>
-<?=doctype('html5')?>
-<?php if($includes['head']) builder_view('web/includes/head'); ?>
-<?php if($includes['header']) builder_view('web/includes/header'); ?>
-<?php if($includes['modalPrepend']) builder_view('web/includes/modal_prepend'); ?>
-<?php if(isset($subPage)) builder_view($subPage); ?>
-<?php if($includes['modalAppend']) builder_view('web/includes/modal_append'); ?>
-<?php if($includes['footer']) builder_view('web/includes/footer'); ?>
-<?php if($includes['tail']) builder_view('web/includes/tail'); ?>
