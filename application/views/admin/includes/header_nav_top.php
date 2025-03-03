30ab4a90c83d480b55e2b7fab3d27ac45a20df69
pensivetraveler <pensivetraveler@gmail.com>
2025-03-04 05:06:36 +0900

    feat: search 기능 제거

--- application/views/admin/includes/header_nav_top.php
+++ application/views/admin/includes/header_nav_top.php
@@ -9,14 +9,14 @@
 
 	<div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
 		<!-- Search -->
-		<div class="navbar-nav align-items-center">
-			<div class="nav-item navbar-search-wrapper mb-0">
-				<a class="nav-item nav-link search-toggler fw-normal px-0" href="javascript:void(0);">
-					<i class="ri-search-line ri-22px scaleX-n1-rtl me-3"></i>
-					<span class="d-none d-md-inline-block text-muted">Search (Ctrl+/)</span>
-				</a>
-			</div>
-		</div>
+<!--		<div class="navbar-nav align-items-center">-->
+<!--			<div class="nav-item navbar-search-wrapper mb-0">-->
+<!--				<a class="nav-item nav-link search-toggler fw-normal px-0" href="javascript:void(0);">-->
+<!--					<i class="ri-search-line ri-22px scaleX-n1-rtl me-3"></i>-->
+<!--					<span class="d-none d-md-inline-block text-muted">Search (Ctrl+/)</span>-->
+<!--				</a>-->
+<!--			</div>-->
+<!--		</div>-->
 		<!-- /Search -->
 
 		<ul class="navbar-nav flex-row align-items-center ms-auto">
