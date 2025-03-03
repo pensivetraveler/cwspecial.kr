ac541af8766e15a4f1693112343970344971c739
pensivetraveler <pensivetraveler@gmail.com>
2025-03-03 11:40:54 +0900

    Feat: myInfo 클래스 추가

--- application/controllers/admin/MyInfo.php
+++ application/controllers/admin/MyInfo.php
@@ -1,8 +1,26 @@
-<?php
+<?php defined('BASEPATH') or exit('No Direct script access allowed');
 
-namespace admin;
+require_once __DIR__ . '/Common.php';
 
-class MyInfo
+class MyInfo extends Common
 {
+	function __construct()
+	{
+		parent::__construct();
 
+		$this->titleList[] = 'MyInfo';
+		$this->addJsVars([
+			'API_URI' => '/api/users',
+		]);
+	}
+
+	public function index()
+	{
+		$tokenData = $this->validateToken();
+		$this->load->model('Model_User');
+		$user_id = $this->Model_User->getData(['user_id'], [
+			'user_id' => $tokenData->user_id,
+		]);
+		$this->edit($user_id);
+	}
 }
