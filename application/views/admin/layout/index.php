0cc99d78ae1c950a67f9a179859cb1be68c2b8d2
pensivetraveler <pensivetraveler@gmail.com>
2025-03-04 02:48:57 +0900

    feat: 빌더 단일화로 안쓰는 view 제거

--- application/views/admin/layout/index.php
+++
@@ -1,9 +0,0 @@
-<?php if(!isset($includes)) $includes = $this->config->config['admin_base_includes_config']; ?>
-<?=doctype('html5')?>
-<?php if($includes['head']) _view('admin/includes/head'); ?>
-<?php if($includes['header']) _view('admin/includes/header'); ?>
-<?php if($includes['modalPrepend']) _view('admin/includes/modal_prepend'); ?>
-<?php if(isset($subPage)) _view($subPage); ?>
-<?php if($includes['modalAppend']) _view('admin/includes/modal_append'); ?>
-<?php if($includes['footer']) _view('admin/includes/footer'); ?>
-<?php if($includes['tail']) _view('admin/includes/tail'); ?>
--- application/views/web/layout/index.php
+++ application/views/web/layout/index.php
@@ -1,9 +1,9 @@
 <?php if(!isset($includes)) $includes = $this->config->config['web_base_includes_config']; ?>
 <?=doctype('html5')?>
-<?php if($includes['head']) _view('web/includes/head'); ?>
-<?php if($includes['header']) _view('web/includes/header'); ?>
-<?php if($includes['modalPrepend']) _view('web/includes/modal_prepend'); ?>
-<?php if(isset($subPage)) _view($subPage); ?>
-<?php if($includes['modalAppend']) _view('web/includes/modal_append'); ?>
-<?php if($includes['footer']) _view('web/includes/footer'); ?>
-<?php if($includes['tail']) _view('web/includes/tail'); ?>
+<?php if($includes['head']) builder_view('web/includes/head'); ?>
+<?php if($includes['header']) builder_view('web/includes/header'); ?>
+<?php if($includes['modalPrepend']) builder_view('web/includes/modal_prepend'); ?>
+<?php if(isset($subPage)) builder_view($subPage); ?>
+<?php if($includes['modalAppend']) builder_view('web/includes/modal_append'); ?>
+<?php if($includes['footer']) builder_view('web/includes/footer'); ?>
+<?php if($includes['tail']) builder_view('web/includes/tail'); ?>
