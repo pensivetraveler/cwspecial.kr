df9013a234a70e37feb85b34ffd64bde272bb5e1
pensivetraveler <pensivetraveler@gmail.com>
2025-03-06 05:49:50 +0900

    Feat: unravel_list 다른 helper 파일로 이동, reformat_bool_type_list 추가

--- application/helpers/builder/builder_base_helper.php
+++ application/helpers/builder/builder_base_helper.php
@@ -1,5 +1,6 @@
 <?php
-if ( ! function_exists('builder_view')) {
+if ( ! function_exists('builder_view'))
+{
 	function builder_view($path, $vars = array(), $return = false)
 	{
 		$CI = &get_instance();
@@ -15,5 +16,33 @@ if ( ! function_exists('get_yn'))
 	function get_yn($bool): string
 	{
 		return $bool?'Y':'N';
+	}
+}
+
+if ( ! function_exists('unravel_list'))
+{
+	function unravel_list($list) : array
+	{
+		$result = [];
+		foreach ($list as $item) {
+			if(is_array($item)) {
+				foreach ($item as $subitem) {
+					$result[] = $subitem;
+				}
+			}else{
+				$result[] = $item;
+			}
+		}
+		return array_values(array_unique($result));
+	}
+}
+
+if ( ! function_exists('reformat_bool_type_list'))
+{
+	function reformat_bool_type_list($list) : array
+	{
+		return array_keys(array_filter($list, function ($value) {
+			return $value === true || $value === 1;
+		}));
 	}
 }
