4816d9b59f78924b3dc77171a60fe2209aed2e60
pensivetraveler <pensivetraveler@gmail.com>
2025-03-10 23:46:46 +0900

    Feat: onload func added

--- public/assets/builder/js/app-page-onload.js
+++ public/assets/builder/js/app-page-onload.js
@@ -4,4 +4,15 @@ $(function() {
     const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl);
     });
+
+	// perfect scroll bar
+	$('.ps').each(function(k,v) {
+		new PerfectScrollbar(v, {
+			wheelPropagation: false
+		});
+	})
+
+	// spinner
+	if(document.getElementById('loader') !== null)
+		document.getElementById('loader').classList.add('d-none');
 })
