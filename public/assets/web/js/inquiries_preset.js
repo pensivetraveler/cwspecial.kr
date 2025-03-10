de3812fa4dc9e4b8a9bde88580f6cfd3c2513cea
pensivetraveler <pensivetraveler@gmail.com>
2025-03-10 23:43:20 +0900

    feat: reply_yn 값에 따라 컬럼 색깔 변화

--- public/assets/web/js/inquiries_preset.js
+++ public/assets/web/js/inquiries_preset.js
@@ -1,4 +1,5 @@
-appPlugins.list.datatable.rowCallback = function(row, data, displayNum, displayIndex, dataIndex) {
-	// data[3] -> Age 컬럼 값이 40 이상이면 행 색깔 변경
-	if(!data.reply_yn) $(row).css('background-color', '#cbcbcb');
+appPlugins.list.datatable = {
+	rowCallback : function(row, data, displayNum, displayIndex, dataIndex) {
+		if(data.reply_yn) $(row).css('background-color', '#cbcbcb');
+	}
 }
