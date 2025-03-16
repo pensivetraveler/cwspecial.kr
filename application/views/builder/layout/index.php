2dda83ba586f3d7d2827697f75d3da6538298814
pensivetraveler <pensivetraveler@gmail.com>
2025-03-17 07:56:28 +0900

    feat: tail 호출 이후 exit

--- application/views/builder/layout/index.php
+++ application/views/builder/layout/index.php
@@ -1,9 +1,11 @@
-<?php if(!isset($includes)) $includes = $this->config->config['base_includes_config']; ?>
-<?=doctype('html5')?>
-<?php if($includes['head']) builder_view("$platformName/includes/head"); ?>
-<?php if($includes['header']) builder_view("$platformName/includes/header"); ?>
-<?php if($includes['modalPrepend']) builder_view("$platformName/includes/modal_prepend"); ?>
-<?php if(isset($subPage)) builder_view($subPage); ?>
-<?php if($includes['modalAppend']) builder_view("$platformName/includes/modal_append"); ?>
-<?php if($includes['footer']) builder_view("$platformName/includes/footer"); ?>
-<?php if($includes['tail']) builder_view("$platformName/includes/tail"); ?>
+<?php
+if(!isset($includes)) $includes = $this->config->config['base_includes_config'];
+echo doctype('html5');
+if($includes['head']) builder_view("$platformName/includes/head");
+if($includes['header']) builder_view("$platformName/includes/header");
+if($includes['modalPrepend']) builder_view("$platformName/includes/modal_prepend");
+if(isset($subPage)) builder_view($subPage);
+if($includes['modalAppend']) builder_view("$platformName/includes/modal_append");
+if($includes['footer']) builder_view("$platformName/includes/footer");
+if($includes['tail']) builder_view("$platformName/includes/tail");
+exit;
