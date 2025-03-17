fcdc3daa7f45ee98ead108fd175d48bce454a89d
pensivetraveler <pensivetraveler@gmail.com>
2025-03-18 04:21:28 +0900

    feat: setup folder add, and view added

--- application/views/setup/add_system_user.php
+++ application/views/setup/add_system_user.php
@@ -6,38 +6,38 @@
 			</div>
 			<div class="card-body">
 				<?php
-				echo form_open_multipart('', [
-					'id' => 'formAuth',
-					'class' => "add-new-record needs-validation form-type-page",
-					'onsubmit' => 'return false',
-				], [
-					'_mode' => $this->router->method,
-					'_event' => '',
-				]);
-				foreach ($formData as $item):
-					?>
-					<div class="row mb-4 form-validation-unit">
-						<?=form_label(ucfirst($item['label']), $item['id'], ['class' => 'col-sm-2 col-form-label'])?>
-						<div class="col-sm-10">
-							<div class="input-group input-group-merge">
-								<?php
+					echo form_open_multipart('', [
+							'id' => 'formAuth',
+							'class' => "add-new-record needs-validation form-type-page",
+							'onsubmit' => 'return false',
+					], [
+							'_mode' => $this->router->method,
+							'_event' => '',
+					]);
+					foreach ($formData as $item):
+				?>
+				<div class="row mb-4 form-validation-unit">
+					<?=form_label(ucfirst($item['label']), $item['id'], ['class' => 'col-sm-2 col-form-label'])?>
+					<div class="col-sm-10">
+						<div class="input-group input-group-merge">
+							<?php
 								echo get_admin_form_ico($item);
-								echo form_textarea(
+								echo form_input(
 									[
+										'type' => $item['type'],
 										'name' => $item['field'],
 										'id' => $item['id'],
-										'rows' => $item['attributes']['rows']
 									],
 									set_admin_form_value($item['field'], $item['default'], null),
 									$item['attributes']
 								);
-								?>
-							</div>
-							<?=get_admin_form_text($item)?>
+							?>
 						</div>
+						<?=get_admin_form_text($item)?>
 					</div>
+				</div>
 				<?php
-				endforeach;
+					endforeach;
 				?>
 				<div class="row">
 					<div class="col-sm-12 text-end">
